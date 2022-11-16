c9fa760c1876b45a7989fe51360b3b3670bfb006
jahcoto <jahcoto@gmail.com>
2022-11-16 17:11:42 -0600

    Adding action

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,36 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
 
 //Import components
-import Layout from "./layout/Layout";
+import Layout from './layout/Layout';
 
 //import pages
-import Index, {loader as ClientesLoader} from "./pages/Index";
-import NewClient from "./pages/NewClient";
+import Index, { loader as ClientesLoader } from './pages/Index';
+import NewClient, { action as newClientAction } from './pages/NewClient';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Layout />,
     children: [
       {
         index: true,
         element: <Index />,
-        loader: ClientesLoader
+        loader: ClientesLoader,
       },
       {
-        path: "/clients/new-client",
+        path: '/clients/new-client',
         element: <NewClient />,
+        action: newClientAction,
       },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -1,6 +1,10 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Form } from 'react-router-dom';
 
-import Form from "../components/Form";
+export function action({ request }) {
+  console.log(request);
+}
+
+import ClientForm from '../components/ClientForm';
 const NewClient = () => {
   const navigate = useNavigate();
 
@@ -11,7 +15,7 @@ const NewClient = () => {
       <div className="flex justify-end">
         <button
           className="bg-blue-800 text-white px-3 py-1"
-          onClick={() => navigate("/")}
+          onClick={() => navigate('/')}
         >
           Regresar
         </button>
@@ -19,15 +23,15 @@ const NewClient = () => {
 
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-10">
         <p className="align-center">Formulario</p>
-        <form>
-          <Form />
+        <Form method="post">
+          <ClientForm />
 
           <input
             type="submit"
-            className="mt-5 w-full bg-blue-800 uppercase font-bold text-white text-lg"
+            className="mt-5 w-full bg-blue-800 uppercase font-bold text-white text-lg cursor-pointer"
             value="Registrar Cliente"
           />
-        </form>
+        </Form>
       </div>
     </>
   );
