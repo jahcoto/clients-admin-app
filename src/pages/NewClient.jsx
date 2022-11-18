abbf9d676731d5dd41e5ec8dda9f239c8659938f
jahcoto <jahcoto@gmail.com>
2022-11-17 19:00:36 -0600

    Validate the email field

--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -3,30 +3,37 @@ import { useNavigate, Form, useActionData } from 'react-router-dom';
 import Error from '../components/Error';
 
 export async function action({ request }) {
-  const formData = await  request.formData();
+  const formData = await request.formData();
 
-  const datos = Object.fromEntries(formData)
-  
+  const datos = Object.fromEntries(formData);
+
   //Validacion
-  const errores = []
-  if(Object.values(datos).includes('')){
-    console.log(datos.email)
-    if (datos.nombre.includes('')){
-      errores.push('El campo nombre es requerido')
-    }else if(datos.empresa === ''){
-      errores.push('El campo empresa es requerido')
-    }else if (datos.email === ''){
-      errores.push('El campo email es requerido')
+  const errores = [];
+  if (Object.values(datos).includes('')) {
+    console.log(datos.email);
+    if (datos.nombre.includes('')) {
+      errores.push('El campo nombre es requerido');
+    } else if (datos.empresa === '') {
+      errores.push('El campo empresa es requerido');
+    } else if (datos.email === '') {
+      errores.push('El campo email es requerido');
     } else if (datos.telefono) {
-      errores.push('El campo telefono es requerido')
+      errores.push('El campo telefono es requerido');
     }
   }
 
-  //Retornar datos
-  if (Object.keys(errores)){
-    return errores
+  let regex = new RegExp(
+    "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])",
+  );
+  const email = formData.get('email');
+  if (!regex.test(email)) {
+    errores.push('El email no es valido!!!');
   }
 
+  //Retornar datos
+  if (Object.keys(errores)) {
+    return errores;
+  }
 }
 
 import ClientForm from '../components/ClientForm';
@@ -51,8 +58,11 @@ const NewClient = () => {
 
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-10">
         <p className="text-center">Formulario</p>
-        {errores?ClientForm.length && errores.map((error, index) => <Error key={index}>{error}</Error> ) : null}
-        <Form method="post">
+        {errores
+          ? ClientForm.length &&
+            errores.map((error, index) => <Error key={index}>{error}</Error>)
+          : null}
+        <Form method="post" noValidate>
           <ClientForm />
 
           <input
